aa699a4ef30879077716a72f851e2c7103e72ca6
ZhiweiZhen <89938005+ZhiweiZhen@users.noreply.github.com>
2023-10-25 22:21:48 -0500

    Add files via upload

---
+++ data_converter/n2gnetcomp/distance/__init__.py
@@ -0,0 +1,12 @@
+"""
+*********
+Distances
+*********
+
+Calculation of distances between graphs.
+"""
+
+from n2gnetcomp.distance.features import *
+import n2gnetcomp.distance.features
+from n2gnetcomp.distance.exact import *
+import n2gnetcomp.distance.exact
---
+++ data_converter/n2gnetcomp/linalg/__init__.py
@@ -0,0 +1,23 @@
+"""
+**************
+Linear Algebra
+**************
+
+Linear algebraic functions, calculations of important matrices.
+"""
+
+from n2gnetcomp.linalg.matrices import *
+import n2gnetcomp.linalg.matrices
+
+from n2gnetcomp.linalg.eigenstuff import *
+import n2gnetcomp.linalg.eigenstuff
+
+from n2gnetcomp.linalg.resistance import *
+import n2gnetcomp.linalg.resistance
+
+from n2gnetcomp.linalg.fast_bp import *
+import n2gnetcomp.linalg.fast_bp
+
+# import helper functions for use in other places
+from n2gnetcomp.linalg.matrices import _flat,_pad,_eps
+from n2gnetcomp.linalg.eigenstuff import _eigs
